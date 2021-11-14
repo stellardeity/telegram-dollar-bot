c19cfa16c58cf290df04aad67f31169dd0ea7c78
antaresofficial <antaresofficial@icloud.com>
2021-11-14 08:59:27 +0500

    some changes

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const TelegramApi = require("node-telegram-bot-api");
-const fetch = require("node-fetch");
+import request from "request";
+import cheerio from "cheerio";
+import TelegramApi from "node-telegram-bot-api";
 const token = "2138642011:AAHQO3s7OleGMT-Tg3-MJPCLBXOk7JtaIiE";
 
 const bot = new TelegramApi(token, { polling: true });
 
-bot.on("message", async ({ text, chat: { id } }) => {
-  if (text === "money") {
-    const res = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
-    const json = await res.json();
-    const usd = json.Valute.USD;
-    bot.sendMessage(id, `${usd.Name}: ${usd.Value}`);
-  }
+const URL = "https://quote.rbc.ru/ticker/59111";
+
+request(URL, (err, res, body) => {
+  const $ = cheerio.load(body);
+  const dollar = $(".chart__info__sum").text();
+
+  bot.on("message", async ({ text, chat: { id } }) => {
+    switch (text) {
+      case "/start":
+        bot.sendMessage(
+          id,
+          "You are welcomed by a bot for tracking the exchange rate. To see the current dollar exchange rate, send /dollar"
+        );
+        break;
+      case "/dollar":
+        bot.sendMessage(id, `Доллар США: ${dollar}`);
+        break;
+      default:
+        bot.sendMessage(
+          id,
+          "To see the current dollar exchange rate, send /dollar"
+        );
+    }
+  });
 });
