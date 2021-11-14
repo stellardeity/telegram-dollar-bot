e81762bb555d7ffd81ee305f50df27a7a47f96a3
antaresofficial <antaresofficial@icloud.com>
2021-11-14 19:04:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,35 +8,24 @@ const URL = "https://quote.rbc.ru/ticker/59111";
 
 request(URL, (err, res, body) => {
   const $ = cheerio.load(body);
-  const userId = 648634090;
+
   let dollar = $(".chart__info__sum").text();
-  let updateDollar = dollar;
-
-  setInterval(() => {
-    dollar = $(".chart__info__sum").text();
-
-    if (updateDollar !== dollar) {
-      updateDollar = dollar;
-      bot.sendMessage(userId, `Current dollar rate is ${dollar}`);
-    }
-  }, 10000);
-
-  bot.on("message", async ({ text }) => {
+  bot.on("message", async ({ text, chat: { id } }) => {
     switch (text) {
       case "/start":
         bot.sendMessage(
-          userId,
+          id,
           "You are welcomed by a bot for tracking the exchange rate. To see the current dollar exchange rate, send /dollar"
         );
         break;
       case "/follow":
-        bot.sendMessage(userId, id);
+        bot.sendMessage(id, id);
         break;
       case "/dollar":
-        bot.sendMessage(userId, `Доллар США: ${dollar}`);
+        bot.sendMessage(id, `Доллар США: ${dollar}`);
         break;
       default:
-        bot.sendMessage(userId, "send /dollar");
+        bot.sendMessage(id, "send /dollar");
     }
   });
 });
